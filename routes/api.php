1d91a9b762aea1ec1542777ee838ba41e92d11f6
bekabadzagua <beka.badzagua@gmail.com>
2022-06-29 12:22:29 +0400

    feat: implement jwt authentication

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 });
+
+Route::post('auth/login', 'AuthController@login');
+Route::post('auth/logout', 'AuthController@logout');
+Route::post('auth/refresh', 'AuthController@refresh');
+Route::post('auth/me', 'AuthController@me');
